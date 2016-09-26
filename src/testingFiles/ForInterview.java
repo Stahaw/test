9737114c96c656d39f8f3592f1b3e779dcc00dee
Stahaw <igorstahaw@gmail.com>
2016-09-26 11:03:17 +0300

    bla bla

---
+++ src/testingFiles/ForInterview.java
@@ -0,0 +1,13 @@
+package testingFiles;
+
+public class ForInterview {
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++){
+			int b = 1;
+			b=b*i;
+			i++;
+			System.out.println(b);
+		}
+	}
+
+}
